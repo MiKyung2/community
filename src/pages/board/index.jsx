69d7987efc7648771562f97a12956d97ccd617f6
jungmikyung <123jmk@naver.com>
2020-07-18 16:51:35 +0900

    error 수정

--- src/pages/board/index.jsx
+++ src/pages/board/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { useObserver, useLocalStore } from "mobx-react";
-import broker from "../../api";
+import BoardAPI from "../../api/board";
 import { Row, Table } from "antd";
 import BoardList from "../../components/Board/List";
 import SearchInput from "../../components/SearchInput";
@@ -11,100 +11,6 @@ const BoardPage = (props) => {
   console.log("board props", props);
 
   return useObserver(() => {
-    const state = useLocalStore(() => {
-      return {
-        columns: [
-          {
-            title: "번호",
-            dataIndex: "number",
-            key: "number",
-          },
-          {
-            title: "제목",
-            dataIndex: "title",
-            key: "title",
-          },
-          {
-            title: "작성자",
-            dataIndex: "writer",
-            key: "writer",
-          },
-          {
-            title: "좋아요",
-            dataIndex: "likeCnt",
-            key: "likeCnt",
-          },
-          {
-            title: "조회수",
-            dataIndex: "viewCnt",
-            key: "viewCnt",
-          },
-          {
-            title: "댓글수",
-            dataIndex: "commentCnt",
-            key: "commentCnt",
-          },
-        ],
-        dataSource: [
-          {
-            key: "0",
-            number: "1",
-            title: "제목1",
-            writer: "작성자1",
-            likeCnt: 123,
-            viewCnt: 123,
-            commentCnt: 123,
-          },
-          {
-            key: "1",
-            number: "2",
-            title: "제목2",
-            writer: "작성자2",
-            likeCnt: 123,
-            viewCnt: 123,
-            commentCnt: 123,
-          },
-        ],
-        page: {
-          total: 11,
-          current: 1,
-        },
-      };
-    });
-
-    const filterLists = ["좋아요순", "댓글순", "조회수순"];
-
-    const onClickFilter = (index) => {
-      // index에 따라 다른 방법으로 정렬
-      if (index === 0) {
-        console.log("좋아요순 clicked!", index);
-      } else if (index === 1) {
-        console.log("댓글순 clicked!", index);
-      } else {
-        console.log("조회수순 clicked!", index);
-      }
-    };
-
-    const onClickTableRow = (record, rowIndex) => {
-      return {
-        onClick: () => {
-          // 해당 Row의 게시판 요청 api
-          console.log("Table Row Clicked!-record", record);
-          console.log("Table Row Clicked!-index", parseInt(rowIndex));
-
-          Router.push("/board/[id]", `/board/${rowIndex}`);
-
-          // state.dataSource.map(item => {
-          //   // Router.push(`/board/[id]', '/board/${item.key}`)
-          //   if(parseInt(rowIndex) === item.key) {
-
-          //     console.log("item.key",item.key)
-          //   }
-          //   })
-        },
-      };
-    };
-
     const state = useLocalStore(() => {
       return {
         columns: [
