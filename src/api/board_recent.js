d444fb7920c5522eb678cfc79809caa06f5b46b8
jungmikyung <123jmk@naver.com>
2020-09-05 17:13:14 +0900

    프로필 연동

--- src/api/board_recent.js
+++ src/api/board_recent.js
@@ -3,7 +3,7 @@ import instance from "./axiosWrapper";
 const BoardRecentAPI = {
   get: async (payload) => {
     try {
-      const res = await instance.get(`board/recent/${payload.id}`);
+      const res = await instance.get(`board/recent/${payload.userId}`);
       return res.data;
     } catch (error) {
       console.error(error);
