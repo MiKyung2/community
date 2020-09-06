e517771c6d7685db6171c217f776405c9f8327dc
jungmikyung <123jmk@naver.com>
2020-09-06 17:22:29 +0900

    글자 넘치는 것 수정

--- src/components/profile/ProfileList.jsx
+++ src/components/profile/ProfileList.jsx
@@ -4,6 +4,9 @@ import { MessageOutlined, LikeOutlined, StarOutlined } from '@ant-design/icons';
 import { useObserver } from 'mobx-react';
 import { AppContext } from "../App/context";
 import { toJS } from "mobx";
+import { BOARDE_TYPE } from "../../utils/enum";
+import ReactHtmlParser from 'react-html-parser';
+import styled from 'styled-components';
 
 const IconText = ({ icon, text }) => (
   <Space>
@@ -12,70 +15,77 @@ const IconText = ({ icon, text }) => (
   </Space>
 );
 
-const BOARDE_TYPE = {
-  FREE: "자유게시판",
-};
-
 const ProfileList = (props) => {
   return useObserver(() => {
     const router = useRouter();
     const global = React.useContext(AppContext);
 
     return (
-      <List
-        loading={props.loading}
-        dataSource={props.dataSource}
-        renderItem={(item) => (
-          <>
-          <List.Item
-            key={item.title}
-            
-            onClick={(e) => {
-              e.preventDefault();
-              router.push("/board/[cate]/[id]", `/board/${item.board_type}/${item.id}`);
-            }}
-            actions={[
-              <div
-                style={{}}
-                onClick={(e) => { 
-                  e.preventDefault() ;
-                  router.push("/profile/[userId]", `/profile/${item.writer}`); 
-                }}
-              >
-                {item.writer == global.state.user.userId ? null : (
-                  <>
-                    <Avatar 
-                      style={{ borderRadius: "50%", border: "1px solid #d9d9d9", marginRight: "10px" }} 
-                      src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" 
-                    />
-                    <span>{item.writer}</span>
-                  </>
-                )}
-                
-              </div>,
-            ]}
-          >
-            <List.Item.Meta
-              title={`[${BOARDE_TYPE[item.board_type]}] ${item.title}`}
-              description={item.contents}
-            />
-            
-          </List.Item>
-          <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <div>
-              <IconText icon={StarOutlined} text={item.commentCnt} key="list-vertical-star-o" />
-              <IconText icon={LikeOutlined} text={item.rowLike} key="list-vertical-like-o" />
-              <IconText icon={MessageOutlined} text={item.viewCount} key="list-vertical-message" />
+      <div className={props.className}>
+        <List
+          loading={props.loading}
+          dataSource={props.dataSource}
+          renderItem={(item) => (
+            <>
+            <List.Item
+              key={item.title}
+              
+              onClick={(e) => {
+                e.preventDefault();
+                router.push("/board/[cate]/[id]", `/board/${item.board_type}/${item.id}`);
+              }}
+              actions={[
+                <div
+                  style={{}}
+                  onClick={(e) => { 
+                    e.preventDefault() ;
+                    router.push("/profile/[userId]", `/profile/${item.writer}`); 
+                  }}
+                >
+                  {item.writer == global.state.user.userId ? null : (
+                    <>
+                      <Avatar 
+                        style={{ borderRadius: "50%", border: "1px solid #d9d9d9", marginRight: "10px" }} 
+                        src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" 
+                      />
+                      <span>{item.writer}</span>
+                    </>
+                  )}
+                  
+                </div>,
+              ]}
+            >
+              <List.Item.Meta
+                title={item.board_type ? `[${BOARDE_TYPE[item.board_type]}]` : "" +  item.title}
+                description={ReactHtmlParser(item.contents)}
+              />
+              
+            </List.Item>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <div>
+                <IconText icon={StarOutlined} text={item.commentCnt} key="list-vertical-star-o" />
+                <IconText icon={LikeOutlined} text={item.rowLike} key="list-vertical-like-o" />
+                <IconText icon={MessageOutlined} text={item.viewCount} key="list-vertical-message" />
+              </div>
+              {item.createdDate}
             </div>
-            {item.createdDate}
-          </div>
-          
-          </>
-        )}
-      >
-      </List>
+            
+            </>
+          )}
+        >
+        </List>
+      </div>
     );
   });
 };
 
-export default ProfileList;
+export default styled(ProfileList)`
+  .ant-list-item-meta-description {
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: pre-wrap;
+    max-height: 41px;
+  }
+`;
---
+++ src/utils/enum.js
@@ -0,0 +1,8 @@
+export const BOARDE_TYPE = {
+  FREE: "자유게시판",
+  NOTICE: "공지사항", 
+  QNA: "G&A", 
+  JOB_OFFER: "구인",
+  JOB_SEARCH: "구직", 
+  SECRET: "비밀게시판",
+};
