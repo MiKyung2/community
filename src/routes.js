fbecbf304cc0883725d4818166fefa8ff74e4aeb
jungmikyung <123jmk@naver.com>
2020-09-06 22:53:31 +0900

    메뉴 권한에 따라 설정

--- src/routes.js
+++ src/routes.js
@@ -3,36 +3,43 @@ const routes = [
     name: "Home",
     url: "/",
     as: "/",
+    role: "",
   },
   {
     name: "자유게시판",
     url: "/board/[cate]",
     as: "/board/free",
+    role: "",
   },
   {
     name: "공지사항",
     url: "/board/[cate]",
     as: "/board/noti",
+    role: "",
   },
   {
     name: "Q&A",
     url: "/board/[cate]",
     as: "/board/qna",
+    role: "",
   },
   {
     name: "구인",
     url: "/board/[cate]",
     as: "/board/recruit",
+    role: "",
   },
   {
     name: "구직",
     url: "/board/[cate]",
     as: "/board/resumes",
+    role: "",
   },
   {
     name: "비밀게시판",
     url: "/board/[cate]",
     as: "/board/secret",
+    role: "A",
   },
   // {
   //   name: "로그인",
